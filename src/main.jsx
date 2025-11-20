5071e122e92d2f3ee5afdcd05152e5e4a61c0eb3
Rachit Deshwal <157193302+RachitDeshwal@users.noreply.github.com>
2025-11-20 20:39:55 +0530

    Remove trailing slash from serverURL

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-export const serverURL = "https://sneha-traders-backend.vercel.app/";
+export const serverURL = "https://sneha-traders-backend.vercel.app";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 
